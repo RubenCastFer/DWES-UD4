5948a8b73b0555cb0f108a512b9824538f79b2dd
RubenCastFer <ruben.castellano.fernandez.al@iespoligonosur.org>
2021-10-15 14:28:18 +0200

    ejercicio 1

--- ejercicio1.php
+++ ejercicio1.php
@@ -8,7 +8,37 @@
 </head>
 <body>
     <?php
+        /*$file = fopen("nombres.txt", "r");
+        do{
+            echo fgets($file), "<br>";
+        }while (feof($file)!=true);
         
+        fclose($file);
+        $file = fopen("nombres.txt", "a");
+        //fwrite($file, "Ruben,172,77,blond,fair,blue,19,male,Tatooine,Human ");
+        fclose($file);
+        */
+        $file = fopen("nombres.txt", "r");
+        $personajes=[];
+
+        echo "<table border='1px solid'>";
+
+        do{
+            $personaje = explode(",", fgets($file));
+
+            echo "</tr>";
+            foreach ($personaje as $dato){
+               
+                    echo "<td>$dato</td>";
+                
+            }
+            echo "</tr>";
+        }while (feof($file)!=true);
+
+        
+        echo "</table>";
+        fclose($file);
+
     ?>
 </body>
 </html>
